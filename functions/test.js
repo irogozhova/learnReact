9c7c6edd6484e2701b04e523092ef719773f0f80
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2019-03-30 15:45:29 +0300

    Added function for computing fibonacci numbers with memoization

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,5 @@
 const expect = require('expect.js');
-const { fib } = require('./tasks');
+const { fib, fibWithCache } = require('./tasks');
 
 describe('task 1 fibonacciSimple', () => {
   it('should compute fibonacci number', () => {
@@ -8,5 +8,16 @@ describe('task 1 fibonacciSimple', () => {
     expect(fib(10)).to.be(55);
     expect(fib(15)).to.be(610);
     expect(() => fib(-1)).to.throwException(/Number is less than zero/);
+  });
+});
+
+describe('task 2 fibonacciWithCache', () => {
+  it('should compute fibonacci number using memoization', () => {
+    expect(fibWithCache(1)).to.be(1);
+    expect(fibWithCache(3)).to.be(2);
+    expect(fibWithCache(10)).to.be(55);
+    expect(fibWithCache(15)).to.be(610);
+    expect(fibWithCache(50)).to.be(12586269025);
+    expect(() => fibWithCache(-1)).to.throwException(/Number is less than zero/);
   });
 });
